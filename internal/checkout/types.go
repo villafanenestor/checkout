9b37c41c2da3219ba5784fc0c55afa44f48a2ef7
Nestor Manjaro <mass.nestor@gmail.com>
2026-04-05 21:21:22 -0500

    se definen structs y tipos

--- internal/checkout/types.go
+++ internal/checkout/types.go
@@ -1,1 +1,17 @@
 package checkout
+
+type Money = int
+
+type Item struct {
+	SKU   string
+	Name  string
+	Price Money
+	Qty   int
+}
+
+type Order struct {
+	ID       string
+	Customer string
+	Items    []Item
+	Meta     map[string]string
+}
